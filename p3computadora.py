ada00b43f86ce1b440cd2224df0fedf77076a15d
AlejandroChalan <lachalan9@utpl.edu.ec>
2026-04-19 16:28:38 +0200

    Partes y precios de una computadora

---
+++ p3computadora.py
@@ -0,0 +1,6 @@
+cpu= float(input("Precio del CPU "))
+teclado= float(input("Precio del teclado "))
+pantalla= float(input("Precio de la pantalla "))
+raton= float(input("Precio del raton "))
+compu= cpu+teclado+pantalla+raton
+print(f"El precio total de su computadora es: {compu} ")
