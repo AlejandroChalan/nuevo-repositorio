d43082ccde26eab6265b17ec680af8a62d990144
AlejandroChalan <lachalan9@utpl.edu.ec>
2026-04-19 17:47:33 +0200

    Juan 25 anios

---
+++ juan25e1.py
@@ -0,0 +1,6 @@
+nombre_persona= "Juan"
+anios= 25
+
+print("Hola, mi nombre es %s y tengo %d años."%(nombre_persona, anios))
+print("Hola, mi nombre es {} y tengo {} años.".format(nombre_persona,anios))
+print(f"Hola, mi nombre es {nombre_persona} y tengo {anios} años.")
