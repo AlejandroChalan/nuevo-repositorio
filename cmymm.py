5fa3cf29d4ef364fb473f45e467e89cff71556c8
AlejandroChalan <lachalan9@utpl.edu.ec>
2026-04-19 16:44:22 +0200

    Calculadora de mm y cm

---
+++ cmymm.py
@@ -0,0 +1,7 @@
+cant= float(input("Ingrese la cantidad de metros que desea transformar "))
+cm= cant*100
+mm= cant*1000
+print(f"{cant} es: ")
+print(f"{cm} centimetros")
+print(f"{mm} milimetros")
+            
