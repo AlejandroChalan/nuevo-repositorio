a80816c2c1b6d95c9f0f817b4f4dd27684c61e46
AlejandroChalan <lachalan9@utpl.edu.ec>
2026-04-26 18:12:09 +0200

    Ej 1 Sem 4

---
+++ Ej1S4.py
@@ -0,0 +1,6 @@
+numero= int(input("Ingrese un numero."))
+if numero >= 1000 and numero <= 9999:
+    print("Su numero tiene 4 digitos y es positivo.")
+else:
+    print("Su numero tiene menos, o mas de 4 digitos")
+    
